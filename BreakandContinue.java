5715d1f02ec72a35d90a26f6e08a3dc37d4affa9
stephen <isaacsteverado9@gmail.com>
2025-07-14 02:10:55 +0100

    final commit

--- BreakandContinue.java
+++ BreakandContinue.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BreakandContinue {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
+            System.out.println(input);
+        }
+    }
 }
--- ForEachLoop.java
+++ ForEachLoop.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Mango", "Orange"};
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
+    }
 }
